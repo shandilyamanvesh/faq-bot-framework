34757090de959f3e3644ef41c910a202657fbaad
tejasbandawane <tejasbandawane5@gmail.com>
2019-11-26 21:01:01 +0530

    add x-frame option in config enviroment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,4 +2,8 @@ Rails.application.configure do
   config.action_cable.allowed_request_origins = [/.*/] # restrict to your staging server's domain + domains where you allow the widget to be embedded
 
   config.eager_load = true
+
+  config.action_dispatch.default_headers = {
+    'X-Frame-Options' => 'ALLOWALL'
+  }
 end
